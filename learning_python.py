b8be76e1e06b6854ea033dde41746f1a39275bf3
Nabil Aziz <aziznabil1@hotmail.com>
2020-02-01 20:31:17 +0000

    j

--- learning_python.py
+++ learning_python.py
@@ -16,7 +16,7 @@ result = musicbrainzngs.search_artists(artist="" + artist, type="group", country
 
 artistid = print(result['artist-list'][0]['id'])
 
-x = musicbrainzngs.browse_releases(artist="" + artistid)
+x = musicbrainzngs.browse_releases(artist="cc197bad-dc9c-440d-a5b5-d52ba2e14234")
 
 print(x)
 #recording-list
