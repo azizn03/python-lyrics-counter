cb6b82a34a89750c87a35da7b10ddf8dbe828bbb
Nabil Aziz <aziznabil1@hotmail.com>
2020-02-02 19:04:36 +0000

    s

--- learning_python.py
+++ learning_python.py
@@ -7,21 +7,21 @@ import random
 import json 
 import ast
 import statistics # calculating the mean value
-from flask import Flask, render_template, request
+# from flask import Flask, render_template, request
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("index.htm")
+# @app.route("/")
+# def home():
+#     return render_template("index.htm")
 
-@app.route('/', methods=['POST'])
-def my_form_post():
-    text = request.form['text']
-    artist = text
-    return artist
+# @app.route('/', methods=['POST'])
+# def my_form_post():
+#     text = request.form['text']
+#     artist = text
+#     return artist
 
-artist = my_form_post()
+# artist = my_form_post()
 
 songslist = []
 countlist = []
@@ -37,12 +37,11 @@ for i in range(5):
 print(listt)
 musicbrainzngs.set_useragent("LyricsWordCount", "1.0", "azizn03",)
 
-#artist = input("Enter Artist Name ")
+artist = input("Enter Artist Name ")
 
 x = musicbrainzngs.search_releases(artist="" + artist, country="GB", status="Offical", format='CD')
 
 for i in range(5):
-    print(x['release-list'][listt[n]]['title'])
     xx = x['release-list'][listt[n]]['title']
     songslist.append(xx)
     n += 1
@@ -62,7 +61,13 @@ if i in range(5):
     n += 1
 
 
-print(statistics.mean(countlist))
+print('The Average wordcount of ' + artist + 'from 5 songs are ' + statistics.mean(countlist))
+print("\n")
+print('The Following Songs have been compared:')
+n = 0
+for i in range(5):
+    print(songslist[n]
+    n += 1
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+# if __name__ == "__main__":
+#     app.run(host='0.0.0.0')
